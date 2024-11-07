19a30e192f243fe85c55a5d9f99668c6b0c8056b
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 10:53:23 +0100

    CES-461-migrate-iopstpblevtdispatcher private endpoint added

--- infra/resources/prod/westeurope/data.tf
+++ infra/resources/prod/westeurope/data.tf
@@ -38,3 +38,14 @@ data "azurerm_key_vault_secret" "fn_eucovidcert_API_KEY_PUBLICIOEVENTDISPATCHER"
   name         = "funceucovidcert-KEY-PUBLICIOEVENTDISPATCHER"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
+
+data "azurerm_virtual_network" "vnet_itn" {
+  name                = "${local.project}-itn-common-vnet-01"
+  resource_group_name = "${local.project}-itn-common-rg-01"
+}
+
+data "azurerm_subnet" "subnet_private_endpoints_itn" {
+  name                 = "io-p-itn-pep-snet-01 "
+  resource_group_name  = data.azurerm_virtual_network.vnet_itn.resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.vnet_itn.name
+}
