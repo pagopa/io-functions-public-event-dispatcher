15e000d3624b4f9c7015712eda5cd76a5290710e
Daniele Manni <dan.manni@gmail.com>
2026-03-12 10:36:44 +0100

    Remove comments and empty files, remove pipeline

--- infra/identity/data.tf
+++ infra/identity/data.tf
@@ -1,6 +1,1 @@
-# data "azurerm_key_vault" "key_vault" {
-#   name                = "${local.project}-kv"
-#   resource_group_name = "${local.project}-sec-rg"
-# }
-
 data "azurerm_client_config" "current" {}
